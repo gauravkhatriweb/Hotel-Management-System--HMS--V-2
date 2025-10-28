6f10a5abbd854ce19cf6f8820507553c8b9b01fa
Gaurav Khatri <gauravkhatriweb@gmail.com>
2025-10-28 14:19:01 +0500

    Initialize backend with Express server and essential dependencies

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import "dotenv/config";
+import cors from 'cors';
+
+const app = express();
+app.use(cors()); // Enable Cross-Origin Resource Sharing
+app.use(express.json()); // Parse JSON request bodies
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('API IS WOKRING!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port http://localhost:${PORT}`);
+}); 
+export default app;
+
+ 
